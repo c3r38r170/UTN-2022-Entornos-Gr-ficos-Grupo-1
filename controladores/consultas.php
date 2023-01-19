6ac03f22e41c6fe4ac1bc83270ad007393e55d92
c3r38r170 <c3r38r170@gmail.com>
2023-01-19 16:13:16 -0300

    Fix for consultas controller, on the pagination mechanism.

--- controladores/consultas.php
+++ controladores/consultas.php
@@ -3,7 +3,7 @@
 require_once(realpath(dirname(__FILE__) . '/../utils/DAOs/consultaDAO.php'));
 
 function searchCon($cons, $offset=0, $limit=10){
-    return search($cons, $offset, $limit);
+    return search($cons, $offset, $limit+1);
  }
 
 
