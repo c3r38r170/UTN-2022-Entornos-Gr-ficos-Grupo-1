e43b1c9e516459298df95b8c6eff6a64ad6ca9df
gturconi <35380017+gturconi@users.noreply.github.com>
2023-01-22 10:32:25 -0300

    Update instanciaDAO.php

--- utils/DAOs/instanciaDAO.php
+++ utils/DAOs/instanciaDAO.php
@@ -3,7 +3,19 @@
 function getInstance($id){
 	$db=new MysqliWrapper();
 
-	$vSql = "SELECT * FROM instancias WHERE id=?";
+	$vSql = "SELECT 
+                 i.id, 
+                 i.fecha, 
+                 i.motivo, 
+                 i.hora_nueva, 
+                 i.cupo, i.enlace,
+                 ie.descripcion 
+            FROM instancias i  
+            INNER JOIN instancias_estados ie 
+            ON i.estado_id=ie.id 
+            WHERE i.id=? AND ie.descripcion 
+             NOT LIKE 
+             'Finalizada'";
 	$rs_result = $db->prepared($vSql,[$id]); 
 	$cons = $rs_result->fetch_array();
 	 
@@ -34,7 +46,7 @@ function addSubscriptor($idUser,$idInstance){
 
 function getSubscription($idUser,$idInstance){
     $db=new MysqliWrapper();
-
+    
     $vSql = "SELECT * FROM suscripciones WHERE estudiante_id=? AND instancia_id=?";
 	$rs_result = $db->prepared($vSql,[$idUser,$idInstance]); 
 	$cons = $rs_result->fetch_array();
