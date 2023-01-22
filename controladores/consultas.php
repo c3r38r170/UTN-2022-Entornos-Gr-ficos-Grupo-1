5920010835713322a84a3c7e6bc83d0944662542
gturconi <35380017+gturconi@users.noreply.github.com>
2023-01-22 18:12:30 -0300

    Update consultas.php

--- controladores/consultas.php
+++ controladores/consultas.php
@@ -1,10 +1,54 @@
 <?php
 
 require_once(realpath(dirname(__FILE__) . '/../utils/DAOs/consultaDAO.php'));
+require_once(realpath(dirname(__FILE__) . '/../utils/DAOs/instanciaDAO.php'));
+require_once(realpath(dirname(__FILE__) . '/../utils/DAOs/usuarioDAO.php'));
+
+session_start();
 
 function searchCon($cons, $offset=0, $limit=10){
     return search($cons, $offset, $limit+1);
  }
 
+
+ if(isset($_POST['ins'])){ 
+    subscribe();
+ }
+
+function subscribe(){
+
+    extract($_REQUEST);    
+    $legajo = $_SESSION['legajo'];    
+    
+    $user = getUser($legajo);        
+    $instance = getInstance($id);
+
+    if(empty($instance)){
+        createInstance($id);     
+        addSubscriptor($user['id'],$instance['id']);
+        ///TO DO: enviar mail al estudiante y al docente
+
+    }                           
+    else        
+        addSubscriptor($user['id'],$instance['id']); 
+        ///TO DO: enviar mail al estudiante
+           
+    $success = "Inscripcion realizada con exito";
+    header("Location: ../consultas.php?success=".urlencode(json_encode($success)));                  
+}
+
+function isSubscribed($idConsult){
+    
+    $instance = getInstance($idConsult);        
+    
+    $legajo = $_SESSION['legajo'];        
+    $user = getUser($legajo);    
+    
+    $subscription = getSubscription($user['id'],$instance['id']);
+
+    if(empty($subscription))
+     return false;
+    return true; 
+}
 
 ?>
