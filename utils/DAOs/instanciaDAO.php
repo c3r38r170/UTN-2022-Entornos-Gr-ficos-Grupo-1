6e68bb222b3bd11cdad9f5f130651e93df8e4f39
gturconi <35380017+gturconi@users.noreply.github.com>
2023-01-25 20:39:16 -0300

    Get most current instance

--- utils/DAOs/instanciaDAO.php
+++ utils/DAOs/instanciaDAO.php
@@ -1,22 +1,27 @@
 <?php
 
+require_once(dirname(__DIR__,1) . '\getDate.php');
+
+//Traemos solo aquellas instancias que correspondan a consultas vigentes
 function getInstance($id){
 	$db=new MysqliWrapper();
-
+    
 	$vSql = "SELECT 
                  i.id, 
                  i.fecha, 
                  i.motivo, 
                  i.hora_nueva, 
                  i.aula_nueva, 
-                 i.cupo, i.enlace,
+                 i.cupo, 
+                 i.enlace,
+                 i.fecha_consulta,
                  ie.descripcion 
             FROM instancias i  
             INNER JOIN instancias_estados ie 
             ON i.estado_id=ie.id 
-            WHERE i.consulta_id=? AND ie.descripcion 
-             NOT LIKE 
-             'Finalizada'";
+            WHERE i.consulta_id=? AND
+            i.fecha_consulta>=CURDATE()";
+
 	$rs_result = $db->prepared($vSql,[$id]); 
 	$cons = $rs_result->fetch_array();
 	 
@@ -25,14 +30,21 @@ function getInstance($id){
 	return $cons;
 }
 
+//Al momento de crear la instancia, guardamos la fecha en la que se va a llevar a cabo la consulta
 function createInstance($id){
     $db=new MysqliWrapper();
-    $vSql = "INSERT INTO instancias (fecha, cupo, estado_id,consulta_id) VALUES (?,?,?,?)";
+
+    $query = "SELECT dia_de_la_semana FROM consultas WHERE id=?";
+    $rs_result = $db->prepared($query,[$id]);
+    $day = mysqli_fetch_array($rs_result);
+	$rs_result->free();
+
+    $vSql = "INSERT INTO instancias (fecha, cupo, estado_id,consulta_id, fecha_consulta) VALUES (?,?,?,?,?)";
     
     date_default_timezone_set('America/Argentina/Buenos_Aires');    
     $date=date('Y/m/d/');
     
-    $db->prepared($vSql,[$date,0,1,$id]);
+    $db->prepared($vSql,[$date,0,1,$id,getWeekDate($day['dia_de_la_semana'])]);
     return $db->insert_id();
 }
 
@@ -62,9 +74,8 @@ function studentCon($id,$offset=0, $limit=10){
         INNER JOIN materia mat ON mat.id=mc.materia_id
         INNER JOIN usuarios u ON u.id=c.profesor_id
         INNER JOIN instancias i ON i.consulta_id=c.id
-        INNER JOIN suscripciones s ON s.estudiante_id=? AND s.instancia_id=i.id
-        INNER JOIN instancias_estados ie on ie.id=i.estado_id
-    WHERE ie.descripcion NOT LIKE 'Finalizada' OR ie.descripcion NOT LIKE 'Bloqueado por profesor'
+        INNER JOIN suscripciones s ON s.estudiante_id=? AND s.instancia_id=i.id        
+    WHERE i.fecha_consulta>=CURDATE()
     LIMIT $limit OFFSET $offset";
 
     $rs_result = $db->prepared($sql,[$id]);
