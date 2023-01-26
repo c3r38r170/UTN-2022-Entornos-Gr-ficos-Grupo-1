062c3b3c776f1ffc3dd0fc98db46a3d211c4076b
franGiangiordano <frangiangiordano@gmail.com>
2023-01-26 17:16:54 -0300

    Update breadcrumbs for different types of users (breadcrumbs.php)

--- template/breadcrumbs.php
+++ template/breadcrumbs.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'utils/usuario-tipos.php';
 
 function generateBreadcrumbs($breadcrumbs){
 	$ul='<ul class="breadcrumbs">';
@@ -19,45 +20,109 @@ function registerBreadcrumbs(){
 	return generateBreadcrumbs($breadcrumbs);
 }
 
-function contactBreadcrumbs(){
-	$breadcrumbs = [array("link" => "ingreso.php","text" => "Home"), array("link" => "#","text" => "Contacto")] ;	
-	return generateBreadcrumbs($breadcrumbs);
-}
-
 function comBreadcrumbs(){
+	if(sessionEsAdministracion())
+	//TODO agregar en "Home" el link del home del administrador
 	$breadcrumbs = [array("link" => "ingreso.php","text" => "Home"), array("link" => "#","text" => "Comisiones")] ;	
 	return generateBreadcrumbs($breadcrumbs);
 }
 
 function matBreadcrumbs(){
+	if(sessionEsAdministracion())
+	//TODO agregar en "Home" el link del home del administrador
 	$breadcrumbs = [array("link" => "ingreso.php","text" => "Home"), array("link" => "#","text" => "Materias")] ;	
 	return generateBreadcrumbs($breadcrumbs);
 }
 
+function userBreadcrumbs(){
+	if(sessionEsAdministracion())
+	//TODO agregar en "Home" el link del home del administrador
+	$breadcrumbs = [array("link" => "ingreso.php","text" => "Home"), array("link" => "#","text" => "Usuarios")] ;	
+	return generateBreadcrumbs($breadcrumbs);
+}
+
 function formMatBreadcrumbs(){
+	if(sessionEsAdministracion())
+	//TODO agregar en "Home" el link del home del administrador
 	$breadcrumbs = [array("link" => "ingreso.php","text" => "Home"),array("link" => "materias.php","text" => "Materias") ,array("link" => "#","text" => "Cargar Materias")] ;	
 	return generateBreadcrumbs($breadcrumbs);
 }
 
 function formComBreadcrumbs(){
+	if(sessionEsAdministracion())
+	//TODO agregar en "Home" el link del home del administrador
 	$breadcrumbs = [array("link" => "ingreso.php","text" => "Home"),array("link" => "comisiones.php","text" => "Comisiones") ,array("link" => "#","text" => "Cargar Comisiones")] ;	
 	return generateBreadcrumbs($breadcrumbs);
 }
 
 function horariosBreadcrumbs(){
-	$breadcrumbs = [array("link" => "ingreso.php","text" => "Home"), array("link" => "#","text" => "Horarios")] ;	
-	return generateBreadcrumbs($breadcrumbs);
-}
-
-function consultasBreadcrumbs(){
-	$breadcrumbs = [array("link" => "ingreso.php","text" => "Home"), array("link" => "#","text" => "Consultas")] ;	
+	if(sessionEsAdministracion())
+	//TODO agregar en "Home" el link del home del administrador
+	$breadcrumbs = [array("link" => "ingreso.php","text" => "Home"), array("link" => "#","text" => "Horarios Consultas")] ;	
 	return generateBreadcrumbs($breadcrumbs);
 }
 
 function preguntasFrecuentesBreadcrumbs(){
-	$breadcrumbs = [array("link" => "ingreso.php","text" => "Home"), array("link" => "#","text" => "Preguntas Frecuentes")] ;	
+	if(sessionEsAdministracion())
+	//TODO agregar en "Home" el link del home del administrador
+		$breadcrumbs = [array("link" => "ingreso.php","text" => "Home"), array("link" => "#","text" => "Preguntas Frecuentes")] ;
+	else if (sessionEsEstudiante())
+		$breadcrumbs = [array("link" => "estudiante.php","text" => "Home"), array("link" => "#","text" => "Preguntas Frecuentes")] ;
+	else if (sessionEsProfesor())
+		$breadcrumbs = [array("link" => "profesor.php","text" => "Home"), array("link" => "#","text" => "Preguntas Frecuentes")] ;
+	else $breadcrumbs = [array("link" => "ingreso.php","text" => "Home"), array("link" => "#","text" => "Preguntas Frecuentes")] ;	
+
 	return generateBreadcrumbs($breadcrumbs);
 }
+
+function misConsultasBreadcrumbs(){
+	if (sessionEsEstudiante())
+		$breadcrumbs = [array("link" => "estudiante.php","text" => "Home"), array("link" => "#","text" => "Mis Consultas")] ;
+	else if (sessionEsProfesor())
+		$breadcrumbs = [array("link" => "profesor.php","text" => "Home"), array("link" => "#","text" => "Mis Consultas")] ;
+		
+	return generateBreadcrumbs($breadcrumbs);
+}
+
+function consultasBreadcrumbs(){
+	if(sessionEsAdministracion())
+	//TODO agregar en "Home" el link del home del administrador
+		$breadcrumbs = [array("link" => "ingreso.php","text" => "Home"), array("link" => "#","text" => "Consultas")] ;
+	else if (sessionEsEstudiante())
+		$breadcrumbs = [array("link" => "estudiante.php","text" => "Home"), array("link" => "#","text" => "Consultas")] ;
+	else if (sessionEsProfesor())
+		$breadcrumbs = [array("link" => "profesor.php","text" => "Home"), array("link" => "#","text" => "Consultas")] ;
+	else $breadcrumbs = [array("link" => "ingreso.php","text" => "Home"), array("link" => "#","text" => "Consultas")] ;	
+
+	return generateBreadcrumbs($breadcrumbs);
+}
+
+
+function contactBreadcrumbs(){
+	if(sessionEsAdministracion())
+	//TODO agregar en "Home" el link del home del administrador
+		$breadcrumbs = [array("link" => "ingreso.php","text" => "Home"), array("link" => "#","text" => "Contacto")] ;
+	else if (sessionEsEstudiante())
+		$breadcrumbs = [array("link" => "estudiante.php","text" => "Home"), array("link" => "#","text" => "Contacto")] ;
+	else if (sessionEsProfesor())
+		$breadcrumbs = [array("link" => "profesor.php","text" => "Home"), array("link" => "#","text" => "Contacto")] ;
+	else $breadcrumbs = [array("link" => "ingreso.php","text" => "Home"), array("link" => "#","text" => "Contacto")] ;	
+
+	return generateBreadcrumbs($breadcrumbs);
+}
+
+function miCuentaBreadcrumbs(){
+	if(sessionEsAdministracion())
+	//TODO agregar en "Home" el link del home del administrador
+		$breadcrumbs = [array("link" => "ingreso.php","text" => "Home"), array("link" => "#","text" => "Mi cuenta")] ;
+	else if (sessionEsEstudiante())
+		$breadcrumbs = [array("link" => "estudiante.php","text" => "Home"), array("link" => "#","text" => "Mi cuenta")] ;
+	else if (sessionEsProfesor())
+		$breadcrumbs = [array("link" => "profesor.php","text" => "Home"), array("link" => "#","text" => "Mi cuenta")] ;	
+
+	return generateBreadcrumbs($breadcrumbs);
+}
+
 
 
 ?>
