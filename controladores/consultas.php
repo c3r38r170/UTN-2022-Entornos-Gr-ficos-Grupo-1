6bd0f9e6de38292efa439c9eaeac3713431dedae
c3r38r170 <c3r38r170@gmail.com>
2023-02-03 19:06:31 -0300

    Code sections in "controladores/consultas.php"

--- controladores/consultas.php
+++ controladores/consultas.php
@@ -8,7 +8,9 @@ require_once(realpath(dirname(__FILE__) . '/../utils/usuario-tipos.php'));
 
 session_start(['read_and_close'=>true]);
 
+// * Uso general
 
+// TODO default limit 11, ver si no afecta en ningun lado
 function searchCon($cons, $offset=0, $limit=10){
     if (sessionEsProfesor()){
         $legajo = $_SESSION['legajo'];
@@ -16,11 +18,17 @@ function searchCon($cons, $offset=0, $limit=10){
         return ConsultaDAO::search($cons, $offset, $limit+1,$user['id']);
     }
     return ConsultaDAO::search($cons, $offset, $limit+1);
- }
+}
 
  function getInst($idCon){
     return InstanciaDAO::getInstance($idCon);
  }
+
+ function getAll($offset=0, $limit=11){
+    return ConsultaDAO::getAll($offset, $limit);
+ }
+ 
+// * Estudiante
 
  function getStudentCon($offset=0, $limit=10){
     $legajo = $_SESSION['legajo'];
@@ -29,49 +37,12 @@ function searchCon($cons, $offset=0, $limit=10){
     return InstanciaDAO::studentCon($user['id'],$offset=0, $limit=10); 
  }
 
- function getTeacherCon($offset=0, $limit=10){
-    $legajo = $_SESSION['legajo'];
-    $user = UsuarioDAO::getUser($legajo);
-
-    return InstanciaDAO::pendingTeacherCon($user['id'],$offset=0, $limit=10); 
- }
-
- function teacherConAssigned(){
-    $legajo = $_SESSION['legajo'];
-    $user = UsuarioDAO::getUser($legajo);
-    
-    return ConsultaDAO::teacherCon($user['id']);
- }
-
- if(isset($_POST['ins'])){ 
-    subscribe();
- }
-
- if(isset($_POST['cancel'])){ 
-    unsubscribe();
- }
-
- if(isset($_POST['confirm'])){ 
-    confirm();
- }
-
- 
  function getSubs($offset=0, $limit=10){
     extract($_REQUEST);    
     return SubscriptionDAO::selectSubscriber($id,$offset=0, $limit=10);
  }
 
- function confirm(){
-    extract($_REQUEST);    
-
-    $instance = InstanciaDAO::getInstance($id);
-    InstanciaDAO::confirmCon($instance['id']);
-    //TO DO: notificar a los estudiantes inscriptos
-
-    header('Location: ../consultas.php');        
- }  
-
-function unSubscribe(){
+ function unSubscribe(){
     extract($_REQUEST);    
     $legajo = $_SESSION['legajo'];    
     
@@ -93,7 +64,6 @@ function unSubscribe(){
     $success = "Usted ya no se encuentra suscrito a la consulta";
     header('Location: ' . strtok($_SERVER['HTTP_REFERER'], '?')."?success=".urlencode(json_encode($success)));               
 }
-
 
 function subscribe(){
 
@@ -139,5 +109,50 @@ function isSubscribed($idConsult){
 
     return !empty($subscription); 
 }
+
+// * Profesor
+
+ function getTeacherCon($offset=0, $limit=10){
+    $legajo = $_SESSION['legajo'];
+    $user = UsuarioDAO::getUser($legajo);
+
+    return InstanciaDAO::pendingTeacherCon($user['id'],$offset=0, $limit=10); 
+ }
+
+ function teacherConAssigned(){
+    $legajo = $_SESSION['legajo'];
+    $user = UsuarioDAO::getUser($legajo);
+    
+    return ConsultaDAO::teacherCon($user['id']);
+ }
+ 
+
+//  * Controlador por redirección 
+
+if(isset($_POST['ins'])){ 
+    subscribe();
+ }
+
+ if(isset($_POST['cancel'])){ 
+    unsubscribe();
+ }
+
+ if(isset($_POST['confirm'])){ 
+    // TODO ver si es profesor
+
+    extract($_REQUEST);
+    // * $confirm es la ID de la instancia; $id, de la consulta
+
+    if(!(int)$confirm){
+        // TODO Añadir robustez a cuando viene 0 pero la instancia existe... según la fecha... quizá esto mate el proposito de mandar la id
+        $confirm=InstanciaDAO::createInstance($id);
+    }
+
+    InstanciaDAO::confirmCon($confirm);
+    //TO DO: notificar a los estudiantes inscriptos
+
+    // TODO volver a donde estaba
+    header('Location: ../consultas.php'); 
+ }
 
 ?>
