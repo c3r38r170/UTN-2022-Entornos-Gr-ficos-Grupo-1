9fd69206c4a6618bc1b04a7a5f822ffbbc39d335
gturconi <35380017+gturconi@users.noreply.github.com>
2023-02-03 22:35:42 -0300

    Error fixed

--- utils/getDate.php
+++ utils/getDate.php
@@ -4,11 +4,11 @@
 //El día se representa mediante un entero, siendo el Lunes el día 1 de la semana
 
 // TODO tildes
+date_default_timezone_set('America/Argentina/Buenos_Aires');
 $days = array('Lunes' => 1, 'Martes' => 2, 'Miercoles' => 3, 'Jueves' => 4, 'Viernes' => 5, 'Sabado' => 6, 'Domingo' => 7);
 
 
-function getWeekDate($day){
-    
+function getWeekDate($day){    
     global $days;
     $day = $days[$day];     
     $dayofweek = date('w', strtotime(date('Y-m-d H:i:s')));
@@ -16,7 +16,7 @@ function getWeekDate($day){
         $result = date('Y-m-d', strtotime((($day+7) - $dayofweek).' day', strtotime(date('Y-m-d H:i:s'))));
     else
         $result = date('Y-m-d', strtotime(($day - $dayofweek).' day', strtotime(date('Y-m-d H:i:s'))));
-     
+    
     return $result; 
 }
 
